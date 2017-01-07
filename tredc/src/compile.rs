be1cd20109d76637a46ec8145011e3034a460b59
Sam Sartor <captainocclusion@gmail.com>
2017-01-06 22:25:32 -0700

    Compile def

--- tredc/src/compile.rs
+++ tredc/src/compile.rs
@@ -10,6 +10,8 @@ use unescape::unescape;
 
 use std::collections::HashMap;
 
+use core::fmt::{self, Formatter, Debug};
+
 #[derive(Debug)]
 enum DefPart {
     STR,
@@ -17,6 +19,7 @@ enum DefPart {
     LIST,
 }
 
+#[derive(Debug)]
 struct CompileData {
     pub defs: Vec<(String, Vec<DefPart>)>,
     pub regexs: Vec<(String, String)>,
@@ -41,6 +44,12 @@ struct BlockDat {
     pub code: BlockBuilder,
 }
 
+impl Debug for BlockDat {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), fmt::Error> {
+        fmt.write_str(&format!("BlockDat {{ id: \"{}\", parent: {:?}, statics: {:?} }}", self.id, self.index, self.statics))
+    }
+}
+
 impl BlockDat {
     pub fn new(index: usize, parent: Option<usize>) -> BlockDat {
         BlockDat {
@@ -63,6 +72,9 @@ enum Value {
     },
     Block {
         index: usize,
+    },
+    StatRef {
+        val: Box<Value>
     }
 }
 
@@ -72,10 +84,41 @@ impl Value {
     }
 }
 
-fn compile_name_expr(dat: &mut CompileData, block: usize, name: &String, args: &Vec<Box<Item>>) {
-    println!("Name Expr: {} {:?}", name, args);
+fn compile_name_expr(dat: &mut CompileData, block: usize, op: &String, args: &Vec<Box<Item>>) {
     let block = &mut dat.blocks[block];
     let code = &mut block.code;
+
+    match &op[..] {
+        // def expression
+        "def" => {
+            let err = format!("\"def\" expr \"def <name> [<str|item|list> ...]\" has invalid args: {:?}", args);
+            // get the new defined tuple/enum's name (first arg)
+            if let [box Item::Name(ref name), ..] = args[..] {
+                let mut parts = Vec::new();
+                for i in &args[1..] {
+                    // for each type in the tuple (remaining args)
+                    if let &box Item::Name(ref ty) = i {
+                        parts.push(match &ty.to_lowercase()[..] {
+                            "str" => DefPart::STR,
+                            "item" => DefPart::ITEM,
+                            "list" => DefPart::LIST,
+                            _ => panic!(err)
+                        });
+                    } else {
+                        panic!(err);
+                    }
+                }
+                // add def
+                dat.defs.push((name.clone(), parts));
+            } else {
+                panic!(err); // TODO no panics
+            }
+        },
+        // stat expression (already handled)
+        "stat" => (),
+        // no other expressions
+        _ => ()
+    }
 
     // TODO
 }
@@ -123,18 +166,12 @@ fn compile_from_iter(dat: &mut CompileData, block: usize, toks: &[Box<Item>]) {
             &box Item::Expr(box Item::Name(ref stat), ref args) if stat == "stat" => {
                 // has form "stat name value;"
 
-                // check name
-                if let [box Item::Name(ref name), ..] = args[..] {
-                    // check has value
-                    if let [_, box ref value, ..] = args[..] {
-                        // add value to statics list
-                        let v = compile_value(dat, block, value).unwrap();
-                        dat.blocks[block].statics.insert(name.clone(), v);  // TODO no unwraps
-                    } else {
-                        panic!(format!("No value for \"stat\": {:?}", args)) // TODO better errors
-                    }
+                // check name and value
+                if let [box Item::Name(ref name), box ref value] = args[..] {
+                    let v = compile_value(dat, block, value).unwrap();
+                    dat.blocks[block].statics.insert(name.clone(), v);  // TODO no unwraps
                 } else {
-                    panic!(format!("No name for \"stat\": {:?}", args))
+                    panic!(format!("\"stat\" expr \"stat <name> <value>\" has invalid args: {:?}", args))
                 }
             },
             _ => ()
@@ -155,6 +192,8 @@ pub fn compile(toks: &[Box<Item>]) {
     let mut dat = CompileData::new();
     dat.blocks.push(BlockDat::new(0, None));
     compile_from_iter(&mut dat, 0, toks);
+
+    println!("{:?}", dat);
 
     // TODO enums, funcs, and final output
 }
